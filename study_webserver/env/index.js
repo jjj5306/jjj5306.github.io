448f8647f850a2afa9afd09efc2ad99aad4c172c
JoJunHwa <jjj5306@naver.com>
2021-08-05 15:23:38 +0900

    til

--- study_webserver/env/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const env = {
-    ...process.env,
-};
-
-module.exports = env;
--- study_webserver/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const userRouter = require('./user');
-
-const router = Router();
-
-router.use('/user', userRouter);
-
-module.exports = router;
